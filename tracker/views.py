c7e830d13aeb2eb63404e9a6d2e8b92a83a596e2
oksana-feshchenko <foksanv@gmail.com>
2023-03-29 15:25:59 +0300

    create index view for index.html

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tracker.models import Child, Parent
+
+
+def index(request):
+    num_children = Child.objects.count()
+    num_parents = Parent.objects.count()
+
+    context = {
+        "num_children": num_children,
+        "num_parents": num_parents,
+    }
+
+    return render(request, "tracker/index.html", context=context)
