1459b4735e243234efe29993c2d8ffb23f03d25e
oksana-feshchenko <foksanv@gmail.com>
2023-03-29 15:27:20 +0300

    create static folder and add it to settings.py

--- vaccination_tracker/settings.py
+++ vaccination_tracker/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "tracker.Parent"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
